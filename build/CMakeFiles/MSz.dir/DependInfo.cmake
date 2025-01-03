2864a4fb0accacf8e81d5c4a42b7c8e3fc064af8
Yuxiao Li <yuxiaoli@login33.chn.perlmutter.nersc.gov>
2025-01-03 10:28:48 -0800

    updated .h files

---
+++ build/CMakeFiles/MSz.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/global/homes/y/yuxiaoli/MSz/include/internal/MSz_CUDA/MSz.cu" "CMakeFiles/MSz.dir/include/internal/MSz_CUDA/MSz.cu.o" "gcc" "CMakeFiles/MSz.dir/include/internal/MSz_CUDA/MSz.cu.o.d"
+  "/global/homes/y/yuxiaoli/MSz/include/api/MSz.cpp" "CMakeFiles/MSz.dir/include/api/MSz.cpp.o" "gcc" "CMakeFiles/MSz.dir/include/api/MSz.cpp.o.d"
+  "/global/homes/y/yuxiaoli/MSz/include/internal/MSz_OMP/MSz_omp.cpp" "CMakeFiles/MSz.dir/include/internal/MSz_OMP/MSz_omp.cpp.o" "gcc" "CMakeFiles/MSz.dir/include/internal/MSz_OMP/MSz_omp.cpp.o.d"
+  "/global/homes/y/yuxiaoli/MSz/include/internal/MSz_Serial/MSz_serial.cpp" "CMakeFiles/MSz.dir/include/internal/MSz_Serial/MSz_serial.cpp.o" "gcc" "CMakeFiles/MSz.dir/include/internal/MSz_Serial/MSz_serial.cpp.o.d"
+  "/global/homes/y/yuxiaoli/MSz/include/internal/MSz_global/MSz_globals.cpp" "CMakeFiles/MSz.dir/include/internal/MSz_global/MSz_globals.cpp.o" "gcc" "CMakeFiles/MSz.dir/include/internal/MSz_global/MSz_globals.cpp.o.d"
+  "/global/homes/y/yuxiaoli/MSz/include/internal/internal.cpp" "CMakeFiles/MSz.dir/include/internal/internal.cpp.o" "gcc" "CMakeFiles/MSz.dir/include/internal/internal.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MSz.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MSz.dir/include/api/MSz.cpp.o"
+  "CMakeFiles/MSz.dir/include/api/MSz.cpp.o.d"
+  "CMakeFiles/MSz.dir/include/internal/MSz_CUDA/MSz.cu.o"
+  "CMakeFiles/MSz.dir/include/internal/MSz_CUDA/MSz.cu.o.d"
+  "CMakeFiles/MSz.dir/include/internal/MSz_OMP/MSz_omp.cpp.o"
+  "CMakeFiles/MSz.dir/include/internal/MSz_OMP/MSz_omp.cpp.o.d"
+  "CMakeFiles/MSz.dir/include/internal/MSz_Serial/MSz_serial.cpp.o"
+  "CMakeFiles/MSz.dir/include/internal/MSz_Serial/MSz_serial.cpp.o.d"
+  "CMakeFiles/MSz.dir/include/internal/MSz_global/MSz_globals.cpp.o"
+  "CMakeFiles/MSz.dir/include/internal/MSz_global/MSz_globals.cpp.o.d"
+  "CMakeFiles/MSz.dir/include/internal/internal.cpp.o"
+  "CMakeFiles/MSz.dir/include/internal/internal.cpp.o.d"
+  "libMSz.a"
+  "libMSz.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/MSz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
